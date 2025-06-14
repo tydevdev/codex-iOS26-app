421abe8e168063ea29460df81baa9657a94c656f
tydevdev <typods05@gmail.com>
2025-06-13 23:02:58 -0500

    Initial commit for iOS app

---
+++ codexTestApp/codexTestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  codexTestAppApp.swift
+//  codexTestApp
+//
+//  Created by Ty DeVito on 6/13/25.
+//
+
+import SwiftUI
+
+@main
+struct codexTestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
